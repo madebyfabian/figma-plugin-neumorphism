7ba2b2df321a3ae57d639949cf1a62fab7f34ab0
zap335213 <beer.fabian@mail.de>
2020-03-03 21:02:31 +0100

    Changed webpack to be able to work with woff2

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin
 
 const WebpackMessages = require('webpack-messages')
 
-// Only dev
 console.clear()
 
 module.exports = (env, argv) => ({
@@ -48,7 +47,7 @@ module.exports = (env, argv) => ({
 			{ test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
 
 			// Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-			{ test: /\.(png|jpg|gif|webp|svg)$/, loader: [{ loader: 'url-loader' }] },
+			{ test: /\.(png|jpg|gif|webp|svg|woff2)$/, loader: [{ loader: 'url-loader' }] },
 
 			{ test: /\.scss$/, use: [ 'vue-style-loader', 'css-loader', 'sass-loader' ] }
 		],
