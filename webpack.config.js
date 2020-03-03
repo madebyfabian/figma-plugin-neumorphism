12baa1d2146b03676cf835a2d64e54488808ac4b
zap335213 <beer.fabian@mail.de>
2020-03-03 13:19:11 +0100

    installed node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,8 @@ module.exports = (env, argv) => ({
 
 			// Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
 			{ test: /\.(png|jpg|gif|webp|svg)$/, loader: [{ loader: 'url-loader' }] },
+
+			{ test: /\.scss$/, use: [ 'vue-style-loader', 'css-loader', 'sass-loader' ] }
 		],
 	},
 
